6cbce64b41f530619629489cb69c9a33eb118f16
alyuto <marcwin208@gmail.com>
2024-05-30 20:15:18 +0700

    add binding in WelcomeActivity

--- app/src/main/java/com/dicoding/drfruithy/view/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/dicoding/drfruithy/view/welcome/WelcomeActivity.kt
@@ -6,16 +6,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.dicoding.drfruithy.R
+import com.dicoding.drfruithy.databinding.ActivityWelcomeBinding
 
 class WelcomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityWelcomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_welcome)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
